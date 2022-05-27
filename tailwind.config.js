8038c89267921f8e2e22bc9cbbfc1cd93f30206a
Tushar <tusharkumar0807@gmail.com>
2022-05-28 00:23:18 +0530

    message

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {
+            screens: {
+                xsm: "340px",
+            },
             fontFamily: {
                 body: ["Play"],
                 worksans: ["Work Sans"],
